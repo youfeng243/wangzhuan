9937d48a36f465674283d60b585f286dabbce03f
游丰(002934) <youfeng@fcbox.com>
2020-02-26 23:51:25 +0800

    完成主体功能

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ ACCOUNT_LIST = [
             '{"Channelid":49480,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"李珍珍2","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"3146","FileStream":null,"AliveLastTime":"/Date(1582529896913)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19373,"AccountCode":"https://qr.alipay.com/fkx18459sjtpb7lmwj2l793?t=1582530061899","pl":0}',
             '{"Channelid":49481,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"游丰3","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"3506","FileStream":null,"AliveLastTime":"/Date(1582528365067)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19373,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX05314IKKOIDA3QNG8F7?t=1582529061618","pl":0}'
         ],
+        'cookie': 'ASP.NET_SessionId=egz0pkbgoamqpd54pcmvmvyx',
         # 开关, True代表可用  False代表不启用
         "start": True
     },
@@ -30,6 +31,7 @@ ACCOUNT_LIST = [
             '{"Channelid":51764,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"8426","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(-62135596800000)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19902,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582721687770","pl":0}',
             '{"Channelid":51765,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"8426","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(-62135596800000)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19902,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582721687770","pl":0}'
         ],
+        'cookie': 'ASP.NET_SessionId=cxvj0newpujo2bklnfxjohbc',
         "start": False
     },
     {
@@ -40,6 +42,7 @@ ACCOUNT_LIST = [
         'open_list': [
             '{"Channelid":51260,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"yf9420","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"9240","FileStream":null,"AliveLastTime":"/Date(1582718941073)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19903,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX07754EGBFRZH7TMZCFD","pl":0}'
         ],
+        'cookie': 'ASP.NET_SessionId=hosfkm0uunxejbsbwkie22tt',
         "start": True
     },
 ]

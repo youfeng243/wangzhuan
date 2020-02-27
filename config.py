4e332fcfa16ff23b07fe11d6f3d9969ec191f31b
游丰(002934) <youfeng@fcbox.com>
2020-02-27 10:42:52 +0800

    去除无用配置

--- config.py
+++ config.py
@@ -12,11 +12,6 @@ ACCOUNT_LIST = [
         "password": "zhen1991",
         "token": 'kVcJcoilMXWIvcpJV7FLONVzneaveDaphs8tAenbG3ovzC73Z8uD+pZ5eZC8H9l1FS+vsYfi9qpyjFpBZZSqQSVx8h2obf+bXJa0ZNiTdFZEE7p7bOldI80qpsvpo+0TyTP9K8+Y45yL/3utJWBq5Q==',
         "alipay": "13302963146.jpeg",
-        'open_list': [
-            '{"Channelid":49479,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"李珍珍1","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(1582631604510)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19373,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582619027140","pl":0}',
-            '{"Channelid":49480,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"李珍珍2","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"3146","FileStream":null,"AliveLastTime":"/Date(1582529896913)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19373,"AccountCode":"https://qr.alipay.com/fkx18459sjtpb7lmwj2l793?t=1582530061899","pl":0}',
-            '{"Channelid":49481,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"游丰3","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"3506","FileStream":null,"AliveLastTime":"/Date(1582528365067)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19373,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX05314IKKOIDA3QNG8F7?t=1582529061618","pl":0}'
-        ],
         'cookie': 'ASP.NET_SessionId=egz0pkbgoamqpd54pcmvmvyx',
         # 开关, True代表可用  False代表不启用
         "start": True
@@ -26,11 +21,6 @@ ACCOUNT_LIST = [
         "password": "zhen1991",
         "token": 'I2gC8ie5Ljsflysd5V2irGWW+gbfz8sh1DMSYNkTHjPlQ8rIP/sdVuEgmAc/PE9lupvk0FsSrYyOJ4IsvhdIDXzAdJa4lNA+6b0HAOECP1QI+Q/OuPMROMvnZ6Rds1kf47ajagpL69vV55tMiFK+7ddCyzVP26bN+WopR1icbQI=',
         "alipay": "13686068426.png",
-        'open_list': [
-            '{"Channelid":51515,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"8426","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(1582726547143)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19902,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582619027140","pl":0}',
-            '{"Channelid":51764,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"8426","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(-62135596800000)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19902,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582721687770","pl":0}',
-            '{"Channelid":51765,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"8426","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"8426","FileStream":null,"AliveLastTime":"/Date(-62135596800000)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19902,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX00684VAPHYFAAV09U61?t=1582721687770","pl":0}'
-        ],
         'cookie': 'ASP.NET_SessionId=cxvj0newpujo2bklnfxjohbc',
         "start": False
     },
@@ -39,9 +29,6 @@ ACCOUNT_LIST = [
         "password": "zhen1991",
         "token": 'VsImDWrWasXLrZpXtGo4V+5fZuYx0FdQTqduR3MCzQJIwr49o8ctpWYLUXKKhK4bK4GR8SxY4qyfHKKsuOxL0bS/G9zdMJck61cxUMEzdcyq031iNPCvpt9vCfL7x7j2',
         "alipay": "13532369240.jpeg",
-        'open_list': [
-            '{"Channelid":51260,"GateID":0,"ChannelType":3,"ChanneCode":"ALIPAY","ChannelName":"yf9420","ChannelRate":0,"ChannelStatus":1,"ChannelOrder":0,"ChannelCardNoLength":null,"ChannelCardPassLength":null,"ChannelCardRules":null,"ChannelRemark":"9240","FileStream":null,"AliveLastTime":"/Date(1582718941073)/","ChannelUseLastTime":"/Date(-62135596800000)/","SXFType":0,"PreOrderMoney":0,"MaxSXF":0,"IsDelete":0,"Memberid":19903,"AccountCode":"HTTPS://QR.ALIPAY.COM/FKX07754EGBFRZH7TMZCFD","pl":0}'
-        ],
         'cookie': 'ASP.NET_SessionId=hosfkm0uunxejbsbwkie22tt',
         "start": True
     },

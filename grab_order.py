14eb2bc01f7214d7d0a005360d5a1124f3ead343
游丰(002934) <youfeng@fcbox.com>
2020-02-27 10:30:29 +0800

    增加获取二维码列表参数

--- grab_order.py
+++ grab_order.py
@@ -24,7 +24,9 @@ class GrabOrder(object):
         self.__session = self.__get_session(self.__username,
                                             self.__password,
                                             self.__token)
-        self.__alipay_url = self.__get_alipay_url(self.__user_info.get("alipay"))
+        self.__alipay_pic = self.__user_info.get("alipay")
+        self.__alipay_pic_path = self.__get_pic_path(self.__alipay_pic)
+        self.__alipay_url = self.__get_alipay_url(self.__alipay_pic_path)
         self.__open_index = 0
         self.__open_list = self.__user_info.get("open_list")
         self.__open_length = len(self.__open_list)
@@ -32,6 +34,15 @@ class GrabOrder(object):
         self.__phone = self.__user_info.get("alipay").split(".")[0]
         self.log.info("当前账户支付宝对应手机号码: {}".format(self.__phone))
         self.__cookie = self.__user_info.get("cookie")
+
+    def __get_pic_path(self, pic_name):
+        system = self.__get_system_info()
+        if system == 'win':
+            pic_path = '.\\picture\\' + pic_name
+        else:
+            pic_path = "./picture/" + pic_name
+
+        return pic_path
 
     # 获得系统版本信息
     def __get_system_info(self):
@@ -43,12 +54,7 @@ class GrabOrder(object):
             return 'linux'
         return 'win'
 
-    def __get_alipay_url(self, picture_name):
-        system = self.__get_system_info()
-        if system == 'win':
-            pic_path = '.\\picture\\' + picture_name
-        else:
-            pic_path = "./picture/" + picture_name
+    def __get_alipay_url(self, pic_path):
         url = decode_qr_code(pic_path)
         self.log.info("当前解析到支付宝链接: url = {}".format(url))
         return url
@@ -321,6 +327,70 @@ class GrabOrder(object):
             self.log.error("当前所有帐户均不可用, 退出抢单: {} url = {}".format(self.__user_id, url))
             os._exit(0)
 
+    def get_qr_list(self):
+        '''
+        获取远程账户列表
+        :return:
+        '''
+        url = 'http://h52h.5188wangzhuan.com/api/v2.0/getQrcodeList?version=2.0'
+
+        headers = {
+            'Host': 'h52h.5188wangzhuan.com',
+            'Connection': 'keep-alive',
+            'Content-Length': '196',
+            'Accept': 'application/json, text/plain, */*',
+            'Origin': 'http://h52h.5188wangzhuan.com',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 9; COL-AL10 Build/HUAWEICOL-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.132 Mobile Safari/537.36 Html5Plus/1.0 (Immersed/35.294117)',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-Requested-With': 'io.dcloud.W2Axx.lh',
+            'Referer': 'http://h52h.5188wangzhuan.com/index.html?v=2.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cookie': self.__cookie
+        }
+
+        post_data = {
+            "token": self.__token,
+        }
+
+        try:
+            resp = requests.post(url=url, headers=headers, data=post_data)
+            if resp is None:
+                self.log.error("当前请求站点异常，退出流程: {}".format(self.__user_id))
+                os._exit(0)
+
+            if resp.status_code != 200:
+                self.log.error("请求站点状态码异常: {} url = {} code = {}".format(
+                    self.__user_id, url, resp.status_code))
+                os._exit(0)
+
+            self.log.info("日志: {} {} {}".format(self.__user_id, url, resp.text))
+
+            json_data = resp.json()
+            if json_data is None:
+                self.log.error("返回数据包异常: {} url = {} json_data = None".format(self.__user_id, url))
+                os._exit(0)
+
+            code = json_data.get("code")
+            if code != 0:
+                self.log.error("请求返回code异常: {} url = {} data = {}".format(self.__user_id, url, resp.text))
+                os._exit(0)
+
+            qrcodes = json_data.get("qrcodes")
+            if not isinstance(qrcodes, list):
+                self.log.error("获取收款账户列表异常: {} url = {} data = {}".format(self.__user_id, url, resp.text))
+                os._exit(0)
+
+            return qrcodes
+
+        except Exception as e:
+            self.log.error("请求判断订单信息异常，退出流程: {}".format(self.__user_id))
+            self.log.exception(e)
+            os._exit(0)
+
+    def upload_gathering(self):
+        pass
+
     def run(self):
         self.log.info("开始启动抢单: {} {} {}".format(self.__phone, self.__user_id, self.__account))
 
@@ -340,3 +410,14 @@ class GrabOrder(object):
             if self.__open_listen_order():
                 self.log.info("开启抢单，休眠20秒: {} {} {}".format(self.__phone, self.__user_id, self.__account))
                 time.sleep(20)
+
+
+if __name__ == '__main__':
+    from logger import Logger
+
+    log = Logger('wangzhuan_test.log').get_logger()
+    from config import ACCOUNT_LIST
+
+    grab = GrabOrder(ACCOUNT_LIST[1], log)
+
+    log.info(grab.get_qr_list())

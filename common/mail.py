c22928ee81587fd47b93bf06a6e973c2aec1c49e
游丰(002934) <youfeng@fcbox.com>
2020-03-02 12:08:00 +0800

    增加邮件多账户功能

--- common/mail.py
+++ common/mail.py
@@ -13,8 +13,7 @@ from email.mime.text import MIMEText
 from common.configer import Configer
 
 
-def send_email(mail_host, mail_user, mail_pass, sender, receiver, content, title):
-    receivers = [receiver]
+def send_email(mail_host, mail_user, mail_pass, sender, receivers, content, title):
     message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
     message['From'] = "{}".format(sender)
     message['To'] = ",".join(receivers)
@@ -44,13 +43,14 @@ def send_mail_by_config(conf_dict, content_dict, title):
     mail_pass = conf_dict.get("mail_pass")
     sender = conf_dict.get("sender")
     receiver = conf_dict.get("receiver")
+    receiver_list = receiver.split(",")
     content = json.dumps(content_dict, ensure_ascii=False, indent=4)
 
-    send_email(mail_host, mail_user, mail_pass, sender, receiver, content, title)
+    send_email(mail_host, mail_user, mail_pass, sender, receiver_list, content, title)
 
 
 def main():
-    send_mail_by_file("../mail.ini", {"测试": "测试"}, "测试呢")
+    send_mail_by_file("../mail.ini", {"蓝海订单邮件提醒测试": "蓝海订单邮件提醒测试"}, "这是蓝海订单测试邮件")
 
 
 if __name__ == '__main__':

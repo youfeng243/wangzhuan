93d116ba6e3d556acc9eaa342885d3e4ddc84d3b
游丰(002934) <youfeng@fcbox.com>
2020-03-02 10:21:54 +0800

    关闭语音提醒

--- play_audio.py
+++ play_audio.py
@@ -11,14 +11,15 @@ import pygame
 
 
 def play_hint_audio():
-    pygame.mixer.init()
-    pygame.mixer.music.load("./audio/7500.mp3")
-    pygame.mixer.music.play(loops=100)
-    while True:
-        if pygame.mixer.music.get_busy():
-            time.sleep(1)
-            continue
-        break
+    return
+    # pygame.mixer.init()
+    # pygame.mixer.music.load("./audio/7500.mp3")
+    # pygame.mixer.music.play(loops=100)
+    # while True:
+    #     if pygame.mixer.music.get_busy():
+    #         time.sleep(1)
+    #         continue
+    #     break
 
 
 if __name__ == '__main__':
